7210cd0d48ceb68d31228333fc25a430c1488ae9
小島悠人 <kojimayuto@mbp.local>
2019-10-10 13:22:20 +0900

    型

---
+++ Pra.scala
@@ -0,0 +1,27 @@
+object pra{
+  def main(args:Array[String]):Unit = {
+    var num: Int = 10
+    println(num)
+
+    val num2 = 100
+    println(num2)
+    // varでは再代入できない
+    // num2 = 10
+
+    var float = 0.1
+    var float = 0.1F
+
+    var t = true
+    var f = false
+
+    val a = 'A'
+    val b = '\u0041'
+
+    val s = "hello"
+    // """で囲うと生の文字列になる
+    val es = """\"\'\\"""
+
+
+
+  }
+}
